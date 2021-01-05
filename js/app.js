d850ca6eaa24d151c93d191f1e632db5e4627446
rahuldev <rahuldev.w@gmail.com>
2021-01-04 22:57:26 -0800

    cursor updates

--- js/app.js
+++ js/app.js
@@ -2,43 +2,112 @@
 
 let follower = document.querySelector('.follower');
 
+let hoverLinks = document.querySelectorAll('.hover');
 
 
-var $ = document.querySelector.bind(document);
-     var $on = document.addEventListener.bind(document);
+const windowW = window.innerWidth;
+const windowH = window.innerHeight;
+const maxLength = Math.max(windowW, windowH);
 
-     var xmouse, ymouse;
-     $on('mousemove', function (e) {
-          xmouse = e.clientX || e.pageX;
-          ymouse = e.clientY || e.pageY;
+const cursorWidth = 40;
+const cursorR = cursorWidth >> 1;
+const cursorDelay = 8;
+
+const buttons = Array.from(document.querySelectorAll('.hover'));
+
+const cursor = {
+	el: document.querySelector('.js-cursor'),
+	x: windowW >> 1,
+	y: windowH >> 1,
+	scaleX: 1,
+	scaleY: 1,
+};
+
+const target = {
+	x: windowW >> 1,
+	y: windowH >> 1,
+	width: cursorWidth,
+	followMouse: true,
+};
+
+const norm = (val, max, min) => (val - min) / (max - min);
+const distanceBetween = (v1, v2) => Math.sqrt((v1.x - v2.x) * (v1.x - v2.x) + (v1.y - v2.y) * (v1.y - v2.y));
+
+const loop = () => {
+	const destX = target.x - cursorR;
+	const destY = target.y - cursorR;
+
+	const newX = cursor.x + ((destX - cursor.x) / cursorDelay);
+	const newY = cursor.y + ((destY - cursor.y) / cursorDelay);
+
+	if (target.followMouse) {
+		const distance = Math.abs(distanceBetween(target, cursor));
+		const scale = norm(distance, maxLength, cursorR);
+		cursor.scaleX = 1 + scale;
+		cursor.scaleY = 1 - scale;
+	} else {
+		const targetScale = target.width / cursorWidth;
+
+		cursor.scaleX += (targetScale - cursor.scaleX) / (cursorDelay / 2);
+		cursor.scaleY = cursor.scaleX;
+	}
+
+	cursor.x = newX;
+	cursor.y = newY;
+
+	cursor.el.style.transform = `translate(${cursor.x}px, ${cursor.y}px) scale(${cursor.scaleX}, ${cursor.scaleY})`;
+
+	requestAnimationFrame(loop);
+};
+
+
+
+const onPointerMove = (e) => {
+	if (!target.followMouse) {
+		return;
+	}
+
+	const pointer = (e.touches && e.touches.length) ? e.touches[0] : e;
+	const { clientX: x, clientY: y } = pointer;
+
+	target.x = x;
+	target.y = y;
+};
+
+const onPointerOver = (e) => {
+	const btn = e.target;
+	const rect = btn.getBoundingClientRect();
+
+	target.followMouse = false;
+	target.x = rect.left + (rect.width >> 1);
+	target.y = rect.top + (rect.height >> 1);
+
+	target.width = Math.max(rect.width, rect.height) + 40;
+};
+
+const onPointerOut = () => {
+	target.followMouse = true;
+	target.width = cursorWidth;
+};
+
+document.body.addEventListener('mousemove', onPointerMove);
+document.body.addEventListener('touchmove', onPointerMove);
+
+buttons.forEach((btn) => {
+	btn.addEventListener('touchstart', onPointerOver);
+	btn.addEventListener('mouseover', onPointerOver);
+
+	btn.addEventListener('touchend', onPointerOut);
+	btn.addEventListener('mouseout', onPointerOut);
+});
+
+loop();
+
+     hoverLinks.forEach( links => {
+          links.addEventListener('mouseover', ()=>{
+               follower.classList.add('link-grow');
+          });
+          links.addEventListener('mouseleave', ()=>{
+               follower.classList.remove('link-grow');
+          })
      });
-
-     var ball =  document.querySelector('.follower');
-     var x = void 0,
-          y = void 0,
-          dx = void 0,
-          dy = void 0,
-          tx = 0,
-          ty = 0,
-          key = -1;
-
-     var followMouse = function followMouse() {
-          key = requestAnimationFrame(followMouse);
-
-          if(!x || !y) {
-               x = xmouse;
-               y = ymouse;
-          } else {
-               dx = (xmouse - x) * 0.125;
-               dy = (ymouse - y) * 0.125;
-               if(Math.abs(dx) + Math.abs(dy) < 0.1) {
-                    x = xmouse;
-                    y = ymouse;
-               } else {
-                    x += dx;
-                    y += dy;
-               }
-          }
-          ball.style.left = x + 'px';
-          ball.style.top = y + 'px';
-     };
